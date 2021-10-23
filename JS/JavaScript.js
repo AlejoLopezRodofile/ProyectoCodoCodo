ce2414ddfd49852366b7c2b143330b8a058e1d20
AlejoLopezRodofile <alejolopezrodofile@gmail.com>
2021-10-23 17:30:25 -0300

    Poryecto V1.0

---
+++ JS/JavaScript.js
@@ -0,0 +1,14 @@
+
+function resumen() {
+    var cantidad = parseInt( document.getElementById("cantidad"),10);
+    
+    alert("Hola");
+    alert(cantidad);
+
+    if(cantidad <= 0){
+        alert("Recuerde que debe seleccionar una cantidad de entradas positivas.");
+    }
+
+}
+
+

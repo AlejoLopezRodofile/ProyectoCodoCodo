bf8ccfb5e95d61f548d0c4e57eae61a6739d89ec
AlejoLopezRodofile <alejolopezrodofile@gmail.com>
2021-10-24 15:50:01 -0300

    V2.0

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,14 +1,32 @@
 
-function resumen() {
-    var cantidad = parseInt( document.getElementById("cantidad"),10);
-    
-    alert("Hola");
-    alert(cantidad);
+var valorTicket=200; 
 
+function resumen() {
+    var cantidad = document.getElementById("cantidad").value;
+    var categoria = document.getElementById("categoria").value;
+    
     if(cantidad <= 0){
-        alert("Recuerde que debe seleccionar una cantidad de entradas positivas.");
+        alert("Recuerde que debe seleccionar una cantidad de entradas positivas y no nulas o vacias.");
     }
 
+    var importe; 
+    switch(categoria){
+        case 'Junior': 
+            importe= calculoDescuento(0.15,cantidad);
+            break;
+        case 'Estudiante':
+            importe= calculoDescuento(0.8,cantidad);
+            break;
+        case 'Trainee':
+            importe= calculoDescuento(0.5,cantidad);
+            break;
+    }
+
+    document.getElementById("importeTotal").value = "Total a pagar: "+ "$" + importe;
 }
 
 
+function calculoDescuento(descuento,cantidad) {
+    
+    return valorTicket*cantidad-(valorTicket*cantidad*descuento); 
+}
